d7a57266543eed81f88f172093696b74d2437087
Behrad87 <bdotz@outlook.com>
2020-12-28 00:54:18 +0330

    Add project files.

---
+++ EFDataAccessLibrary/Model/Email.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFDataAccessLibrary.Model
+{
+    public class Email
+    {
+        public int Id { get; set; }
+
+        
+        [Required]
+        [MaxLength(150)]
+        //[Column(TypeName ="varchar(10) ")]
+        public string EmailAddress { get; set; }
+
+
+    }
+}
---
+++ EFDataAccessLibrary/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFDataAccessLibrary.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LstName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        public List<Email> Emails { get; set; } = new List<Email>();
+    }
+}
